d54e75e7c0b3f31d7380bdebd4fa0e5b2384e9a9
Mark Kovacs <64911445+kovacsmarkakos@users.noreply.github.com>
2020-09-08 13:46:58 +0200

    Initial commit

---
+++ src/components/Display.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Display = () => {
+  return (
+    <h1>12425</h1>
+  )
+}
+
+export default Display
---
+++ src/components/Input.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Input = () => {
+  return (
+    <div className="button-container">
+      <button className="button">C</button>
+      <button className="button">+-</button>
+      <button className="button">%</button>
+      <button className="button">/</button>
+      <button className="button">7</button>
+      <button className="button">8</button>
+      <button className="button">9</button>
+      <button className="button">x</button>
+      <button className="button">4</button>
+      <button className="button">5</button>
+      <button className="button">6</button>
+      <button className="button">-</button>
+      <button className="button">1</button>
+      <button className="button">2</button>
+      <button className="button">3</button>
+      <button className="button">+</button>
+      <button className="button">0</button>
+      <button className="button">.</button>
+      <button className="button">.</button>
+      <button className="button">=</button>
+    </div>
+  )
+}
+
+export default Input
